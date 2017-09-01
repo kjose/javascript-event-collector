76149661f3cd0e55e2e622eb4dc9dee88319e512
Kévin JOSE <kevin.jose.92@gmail.com>
2017-09-01 14:51:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         EventElements Collector
+// @name         Javascript X Collector
 // @version      0.1
-// @description  Register all events of an element in a collection
+// @description  Register all events in a collection
 // @author       Kévin José
 // @match        http://*/*
 // @match        https://*/*
@@ -9,43 +9,88 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
 
-    // Set filters for sepcific elements
-    // ex: var filterSelector = '.exlink';
-    // ex: var filterType = 'mouseover';
-    var filterSelector = '#menu_main .menu_item .exlink';
-    var filterType = 'mouseover';
-    // True if you want to prevent the adding of the listener to the filtered element
-    var disableListenerForElementsCatched = true;
-    // True if you want to launch the debugger when an event listener is added to filtered element
-    var debugMode = true;
+    /**
+     * LIBRARY window
+     */
 
-    function path(el, fullpath){
-        var names = [];
-        fullpath = fullpath || false;
-        while (el.parentNode){
-            if (el.id){
-                names.unshift('#'+el.id);
-                break;
-            }else{
-                if (el==el.ownerDocument.documentElement) names.unshift(el.tagName);
-                else{
-                    if (el.className) {
-                        names.unshift('.'+el.className.replace(' ', '.'));
-                    } else if (fullpath) {
-                        for (var c=1,e=el;e.previousElementSibling;e=e.previousElementSibling,c++);
-                        names.unshift(el.tagName+":nth-child("+c+")");
-                    }
-                }
-                el=el.parentNode;
-            }
-        }
-        var separator = fullpath ? " > " : " ";
-        return names.join(separator);
-    }
+    // Init widnow extends objects
+    window.EventDocumentCollector = [];
+    window.EventCollector = [];
+    window.XHRCollector = [];
+    window.TimeoutCollector = [];
+    window.IntervalCollector = [];
 
-    window.EventCollectorElements = [];
+    window.cleanAllEvents = function() {
+        var i;
+        for(i=0; i<EventCollector.length; i++) EventCollector[i].element.removeEventListener(EventCollector[i].type, EventCollector[i].listener, EventCollector[i].useCapture);
+        for(i=0; i<EventDocumentCollector.length; i++) EventDocumentCollector[i].element.removeEventListener(EventDocumentCollector[i].type, EventDocumentCollector[i].listener, EventDocumentCollector[i].useCapture);
+        window.EventCollector = window.EventDocumentCollector = [];
+    };
+
+    window.cleanAllTimeout = function() {
+        for(var i=0; i<TimeoutCollector.length; i++) clearTimeout(TimeoutCollector[i].trigger);
+        window.TimeoutCollector = [];
+    };
+
+    window.cleanAllInterval = function() {
+        for(var i=0; i<IntervalCollector.length; i++) clearInterval(IntervalCollector[i].trigger);
+        window.IntervalCollector = [];
+    };
+
+    window.cleanAllXHR = function() {
+        for(var i=0; i<XHRCollector.length; i++) XHRCollector[i].abort();
+        window.XHRCollector = [];
+    };
+
+    window.cleanAll = function() {
+        window.cleanAllEvents();
+        window.cleanAllTimeout();
+        window.cleanAllInterval();
+        window.cleanAllXHR();
+    };
+
+    /**
+     * CORE
+     */
+
+    // This code must be loaded before all js code of website
+    console.log('JS starting collecting events ...');
+
+    // Parameters
+    var filterSelector = '';
+    var filterType = '';
+    var debugMode = false; // option to enable debug mode (will open debugger when listener attached)
+    var disableListenerForElementsCatched = false; // option to disable the listener for elements matching with filterSelector and filterType
+
+    // Replace method setTimeout
+    var oldAddSetTimeout = window.setTimeout;
+    window.setTimeout = function(fn, delay) {
+        window.TimeoutCollector.push({
+            trigger: oldAddSetTimeout.apply(this, arguments),
+            fn: fn,
+            delay: delay
+        });
+    };
+
+    // Replace method setTimeout
+    var oldAddSetInterval = window.setInterval;
+    window.setInterval = function(fn, delay) {
+        window.IntervalCollector.push({
+            trigger: oldAddSetInterval.apply(this, arguments),
+            fn: fn,
+            delay: delay
+        });
+    };
+
+    // Replace method for sending XmlHttpRequest
+    var oldXHRSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function() {
+        XHRCollector.push(this);
+        oldXHRSend.apply(this, arguments);
+    };
+
+    // Replace method addEventListener for DOM elements
     var oldAddEventListener = Element.prototype.addEventListener;
     Element.prototype.addEventListener = function(type, listener, useCapture) {
         var selector = path(this);
@@ -58,7 +103,7 @@
             useCapture: useCapture
         };
         if (selector.indexOf(filterSelector) !== -1 && type.indexOf(filterType) !== -1) {
-            window.EventCollectorElements.push(data);
+            window.EventCollector.push(data);
             if (debugMode) {
                 debugger;
             }
@@ -69,5 +114,54 @@
             oldAddEventListener.apply(this, arguments);
         }
     };
+
+    // Replace method addEventListener for Window | Document elements
+    function resetEventListenerMethod(el) {
+        var oldFn = el.addEventListener;
+        el.addEventListener = function(type, listener, useCapture) {
+            window.EventDocumentCollector.push({
+                element: el,
+                type: type,
+                listener: listener,
+                useCapture: useCapture
+            });
+            oldFn.call(el, type, listener, useCapture);
+        };
+    }
+    resetEventListenerMethod(window);
+    resetEventListenerMethod(document);
+
+
+    // Function
+    function path(el, fullpath){
+        var names = [];
+        fullpath = fullpath || false;
+        while (el.parentNode){
+            if (el.id){
+                names.unshift('#'+el.id);
+                break;
+            }else{
+                if (el==el.ownerDocument.documentElement) names.unshift(el.tagName);
+                else{
+                    var c = 1;
+                    var s = el.previousElementSibling;
+                    while (s){
+                        // if (s.className == el.className) c++;
+                        c++;
+                        s = s.previousElementSibling;
+                    }
+                    if (el.className) {
+                        names.unshift('.'+el.className.replace(' ', '.')+":nth-child("+c+")");
+                    } else if (fullpath) {
+                        names.unshift(el.tagName+":nth-child("+c+")");
+                    }
+                }
+                el=el.parentNode;
+            }
+        }
+        var separator = fullpath ? " > " : " ";
+        return names.join(separator);
+    }
+
 
 })();
